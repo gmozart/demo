fcbb86b14d3eead121f90f8f78dd1f05a08f9f04
Gleison Mozart <mozart.gleison@gmail.com>
2021-07-27 01:36:54 -0300

    Finishing project with PUT

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,13 +2,18 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.filechooser.FileSystemView;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
+    private FileSystemView studentOptional;
 
     @Autowired
     public StudentService(StudentRepository studentRepository) {
@@ -46,6 +51,32 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+
+
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student With id " +studentId+
+                        " does not exist"));
+                if(name != null && name.length() > 0 && !Objects.equals(student.getName(),name)){
+
+                     student.setName(name);
+
+                }
+                if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+
+                    student.setEmail(email);
+
+                }else{
+
+            throw  new IllegalArgumentException("email taken");
+        }
+        student.setEmail(email);
+        
+       
 
 
     }
